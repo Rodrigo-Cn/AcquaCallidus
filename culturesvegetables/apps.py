784b6ab60556c4a053d4462c171d646a154bb0ce
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-03-23 21:09:15 -0300

    build: Estrutura inicial do projeto

---
+++ culturesvegetables/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CulturesvegetablesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'culturesvegetables'
---
+++ meteorologicaldata/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MeteorologicaldataConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'meteorologicaldata'
