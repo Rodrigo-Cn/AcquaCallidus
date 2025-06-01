e76d5dc37fc89d5772da823b98efaa27b8c4cf73
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-06-01 13:58:18 -0300

    feat: Adiciona o javascript da home

---
+++ static/js/pages/home.js
@@ -0,0 +1,39 @@
+var userMenuDiv = document.getElementById("userMenu");
+var userMenu = document.getElementById("userButton");
+var navMenuDiv = document.getElementById("nav-content");
+var navMenu = document.getElementById("nav-toggle");
+
+document.onclick = check;
+
+function check(e) {
+    var target = (e && e.target) || (event && event.srcElement);
+
+    if (!checkParent(target, userMenuDiv)) {
+        if (checkParent(target, userMenu)) {
+            if (userMenuDiv.classList.contains("invisible")) {
+                userMenuDiv.classList.remove("invisible");
+            } else { userMenuDiv.classList.add("invisible"); }
+        } else {
+            userMenuDiv.classList.add("invisible");
+        }
+    }
+
+    if (!checkParent(target, navMenuDiv)) {
+        if (checkParent(target, navMenu)) {
+            if (navMenuDiv.classList.contains("hidden")) {
+                navMenuDiv.classList.remove("hidden");
+            } else { navMenuDiv.classList.add("hidden"); }
+        } else {
+            navMenuDiv.classList.add("hidden");
+        }
+    }
+
+}
+
+function checkParent(t, elm) {
+    while (t.parentNode) {
+        if (t == elm) { return true; }
+        t = t.parentNode;
+    }
+    return false;
+}
