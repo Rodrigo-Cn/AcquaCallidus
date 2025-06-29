3a70ead16af3700d66ea1432aa6fa5ff69ade5ea
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-06-29 16:50:40 -0300

    feat: Adiciona transição nos toasts

--- static/js/toast.js
+++ static/js/toast.js
@@ -1,7 +1,9 @@
-setTimeout(() => {
-    const toast = document.getElementById('toast');
-    if (toast) {
-    toast.style.opacity = '0';
-    setTimeout(() => toast.remove(), 300);
-    }
-}, 5000);
+document.addEventListener('DOMContentLoaded', () => {
+    const toasts = document.querySelectorAll('#toast > div');
+    toasts.forEach(toast => {
+    setTimeout(() => {
+        toast.style.opacity = '0';
+        setTimeout(() => toast.remove(), 500);
+    }, 5000);
+    });
+});
