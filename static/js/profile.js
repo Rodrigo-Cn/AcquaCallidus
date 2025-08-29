b3748a25e997f859707632c5cc84c9d2ca357556
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-08-28 21:14:19 -0300

    feat: Adiciona template de modificações de perfil

---
+++ static/js/profile.js
@@ -0,0 +1,13 @@
+function closeModal() {
+document.getElementById('profileModal').classList.add('hidden');
+}
+function openModal() {
+document.getElementById('profileModal').classList.remove('hidden');
+}
+
+function openPasswordModal() {
+document.getElementById('passwordModal').classList.remove('hidden');
+}
+function closePasswordModal() {
+document.getElementById('passwordModal').classList.add('hidden');
+}
